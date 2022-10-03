37012a06e507d3bc8aa73821210b9f20e13f523e
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-10-02 23:04:03 -0400

    it seems debounce search finally compile

--- pages/borrow/index.tsx
+++ pages/borrow/index.tsx
@@ -3,14 +3,40 @@ import LayoutRedesign from '../../components/LayoutRedesign/LayoutRedesign';
 import HoneyTable from '../../components/HoneyTable/HoneyTable';
 import { ColumnType } from 'antd/lib/table';
 import * as style from '../../styles/markets.css';
-import debounce from 'lodash';
 import useLoanFlowStore from "../../store/loanFlowStore";
-
-
-const network = 'devnet'; // change to dynamic value
+import { MarketTableRow } from "../../types/markets";
+import { ChangeEvent, useCallback, useState } from "react";
+import _ from "lodash";
 
 
 const Markets: NextPage = () => {
+  const [tableData, setTableData] = useState<MarketTableRow[]>([]);
+  const [tableDataFiltered, setTableDataFiltered] = useState<MarketTableRow[]>(
+    []
+  );
+  const onSearch = (searchTerm: string): MarketTableRow[] => {
+    if (!searchTerm) {
+      return [...tableData];
+    }
+    const r = new RegExp(searchTerm, 'gmi');
+    return [...tableData].filter(row => {
+      return r.test(row.name);
+    });
+  };
+
+  const debouncedSearch = useCallback(
+    _.debounce((criteria: string) => {
+      setTableDataFiltered(onSearch(criteria))
+    }, 500),
+    [])
+
+  const handleSearchInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      debouncedSearch(value);
+    },
+    []
+  );
 
 
   return (
