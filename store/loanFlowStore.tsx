5ec0c6554d2665597e923d0c1f89d5b533d74005
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-10-06 22:10:34 -0400

    add workflow to state to distinguish deposit NFT and borrow/withdraw

--- store/loanFlowStore.tsx
+++ store/loanFlowStore.tsx
@@ -4,9 +4,11 @@ interface LoanFlowState {
   HERC20ContractAddr: string
   NFTId: string
   isLoading: boolean
+  workflow: string
   setIsLoading: (isLoading: boolean) => void
   setNFTId: (NFTId: string) => void
   setHERC20ContractAddr: (HERC20ContractAddr: string) => void
+  setWorkflow: (workflow: string) => void
   reset: () => void
 }
 
@@ -14,12 +16,15 @@ const useLoanFlowStore = create<LoanFlowState>()((set) => ({
   HERC20ContractAddr: "",
   NFTId: "",
   isLoading: false,
+  workflow: "",
   setIsLoading: (isLoading: boolean) => set((state) => ({isLoading: isLoading})),
   setNFTId: (NFTId: string) => set((state) => ({NFTId: NFTId})),
   setHERC20ContractAddr: (HERC20ContractAddr: string) => set((state) => ({HERC20ContractAddr: HERC20ContractAddr})),
+  setWorkflow: (workflow: string) => set((state) => ({workflow: workflow})),
   reset: () => set(() => ({
     HERC20ContractAddr: "",
     NFTId: "",
+    workflow: "",
     isLoading: false,
   })),
 }))
