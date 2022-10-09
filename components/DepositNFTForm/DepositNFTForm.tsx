cf3d37de091f1858358e887b919427c5adea8f49
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-10-09 18:15:29 -0400

    finish porting

--- components/DepositNFTForm/DepositNFTForm.tsx
+++ components/DepositNFTForm/DepositNFTForm.tsx
@@ -1,27 +1,23 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
-import Image from 'next/image';
 
 import * as styles from './DepositNFTForm.css';
 import { formatNumber } from '../../helpers/format';
 import HoneyButton from 'components/HoneyButton/HoneyButton';
-import HexaBoxContainer from 'components/HexaBoxContainer/HexaBoxContainer';
 import NftList from '../NftList/NftList';
 import { DepositNFTProps } from './types';
-import { toastResponse } from 'helpers/loanHelpers';
-import imagePlaceholder from 'public/images/imagePlaceholder.png';
-import { hAlign } from 'styles/common.css';
-import { questionIcon } from 'styles/icons.css';
 import useToast from 'hooks/useToast';
-import cs from 'classnames';
 import useDisplayStore from "../../store/displayStore";
 import SidebarScroll from "../SidebarScroll/SidebarScroll";
 import useLoanFlowStore from "../../store/loanFlowStore";
 import { LoanWorkFlowType } from "../../types/workflows";
 import { getContractsByHTokenAddr } from "../../helpers/generalHelper";
 import { UserContext } from "../../contexts/userContext";
-import { useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { useFetchNFTByUserByCollection, useIsNFTApproved } from "../../hooks/useNFT";
 import { useGetNFTPrice } from "../../hooks/useHtokenHelper";
+import { depositNFTCollateral } from "../../hooks/useHerc20";
+import { queryKeys } from "../../helpers/queryHelper";
+import getDepositNFTApproval from "../../hooks/useERC721";
 
 const {format: f, formatPercent: fp, formatERC20: fs, parse: p} = formatNumber;
 
@@ -31,7 +27,7 @@ const DepositNFTForm = (props: DepositNFTProps) => {
   const queryClient = useQueryClient();
   const walletPublicKey: string = currentUser?.get("ethAddress") || ""
   const HERC20ContractAddress = useLoanFlowStore((state) => state.HERC20ContractAddr)
-  const {nftContractAddress, htokenHelperContractAddress, unit} = getContractsByHTokenAddr(HERC20ContractAddress)
+  const {nftContractAddress} = getContractsByHTokenAddr(HERC20ContractAddress)
   const setWorkflow = useLoanFlowStore((state) => state.setWorkflow)
   const [selectedNft, setSelectedNft] = useState<NFT | null>(null);
   const {toast, ToastComponent} = useToast();
@@ -48,56 +44,80 @@ const DepositNFTForm = (props: DepositNFTProps) => {
       setNFTState('WAIT_FOR_APPROVAL')
   }, [selectedNft]);
 
+  useEffect(() => {
+    if (isLoadingNFT || isLoadingApproval) {
+      toast.processing()
+    } else {
+      toast.clear()
+    }
+  }, [isLoadingNFT, isLoadingApproval]);
+
   const buttonTitle = () => {
     if (nftState == 'WAIT_FOR_APPROVAL') return 'Approve';
     else if (nftState == 'WAIT_FOR_DEPOSIT') return 'Deposit NFT';
-    else return 'View position';
+    else return 'Deposit finished';
   };
 
-  // useEffect(()=> {
-  //   toast.processing()
-  //   toast.success("hello")
-  // },
-  //   []);
+  const depositCollateral = async () => {
+    await depositNFTCollateral(
+      HERC20ContractAddress,
+      selectedNft?.tokenId || ""
+    ).then(() => {
+      console.log('deposit collateral');
+      setSelectedNft(null)
+      setNFTState('FINISH_DEPOSIT');
+    });
+  };
 
+  const depositNFTMutation = useMutation(depositCollateral)
+
+  const onClick = async () => {
+    try {
+      toast.processing()
+      if (nftState == 'WAIT_FOR_APPROVAL') {
+        await getApprovalMutation.mutateAsync()
+        await queryClient.invalidateQueries(queryKeys.NFTApproval(nftContractAddress, HERC20ContractAddress, selectedNft?.tokenId || ""))
+      } else if (nftState == 'WAIT_FOR_DEPOSIT') {
+        await depositNFTMutation.mutateAsync()
+        await queryClient.invalidateQueries(queryKeys.listUserNFTs(walletPublicKey, nftContractAddress))
+        await queryClient.invalidateQueries(queryKeys.listUserCoupons(HERC20ContractAddress, walletPublicKey))
+      } else if (nftState === 'FINISH_DEPOSIT') {
+        setSelectedNft(null)
+      }
+      toast.success('Successful! Transaction complete');
+    } catch (err) {
+      console.error(err);
+      toast.error('Sorry! Transaction failed');
+    } finally {
+    }
+  };
+
+  const getApproval = async () => {
+    await getDepositNFTApproval(
+      nftContractAddress,
+      HERC20ContractAddress,
+      selectedNft?.tokenId || ""
+    ).then(() => {
+      console.log('get NFT Approval');
+    }).catch(function (err) {
+      console.error('get NFT Approval fail');
+      throw err;
+    });
+  };
+
+  const getApprovalMutation = useMutation(getApproval)
 
   // set selection state and render (or not) detail nft
   const selectNFT = (nft: NFT) => {
     setSelectedNft(nft);
   };
 
-
-  const handleDepositNFT = async () => {
-    // if (selectedNft && selectedNft.mint.length < 1)
-    //   return toastResponse('ERROR', 'Please select an NFT', 'ERROR');
-    // if (selectedNft && selectedNft.mint.length > 1)
-    //   await executeDepositNFT(selectedNft.mint, toast);
-  };
-
-  // const availableNFTs: Array<NFT> = [
-  //   {
-  //     id: "1",
-  //     name: "abc",
-  //     image: "/nfts/bayc.jpg",
-  //     tokenId: "1",
-  //     contractAddress: "xx"
-  //
-  //   },
-  //   {
-  //     id: "2",
-  //     name: "eye",
-  //     image: "/nfts/bayc.jpg",
-  //     tokenId: "3",
-  //     contractAddress: "xx"
-  //   }
-  // ]
-
-
   const renderContent = () => {
     return (
       <>
         <div className={styles.newBorrowingTitle}>Choose NFT</div>
         <NftList
+          //todo use price from oracle
           nftPrice={100}
           data={availableNFTs}
           selectNFT={selectNFT}
@@ -130,7 +150,7 @@ const DepositNFTForm = (props: DepositNFTProps) => {
           <HoneyButton
             variant="primary" isFluid
             disabled={selectedNft == null}
-            onClick={handleDepositNFT}>
+            onClick={onClick}>
             <>
               {buttonTitle()}
             </>
