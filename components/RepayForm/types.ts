857fcce8f7a17bdd352b4d400a75378c62c9df7b
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-10-18 22:52:02 -0400

    add mock repay form

--- components/RepayForm/types.ts
+++ components/RepayForm/types.ts
@@ -1,14 +1,3 @@
 export type RepayProps = {
-  collectionId?: string;
-  openPositions?: any;
-  nftPrice?: number;
-  userAllowance: number;
   userDebt: number;
-  userUSDCBalance: number;
-  loanToValue: number;
-  availableNFTs: any;
-  executeWithdrawNFT: (mint: string, toast: any) => void;
-  executeRepay: (val: number, toast: any) => void;
-  hideMobileSidebar?: () => void;
-  changeTab?: (tabKey: string) => void;
 };
