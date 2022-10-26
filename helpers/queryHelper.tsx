e35d91797cf52600713170ad57c4129c4baba9b4
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-10-25 23:15:15 -0400

    add market total supply

--- helpers/queryHelper.tsx
+++ helpers/queryHelper.tsx
@@ -8,6 +8,7 @@ export const queryKeys = {
   maxBorrow: (HERC20ContractAddress: string) => ['nft', 'maxBorrow', HERC20ContractAddress] as const,
   maxBorrowFromNFT: (HERC20ContractAddress: string, ERC721ContractAddress: string, userAddress: string, NFTTokenId: string) => ['nft', 'maxBorrow', HERC20ContractAddress, ERC721ContractAddress, userAddress, NFTTokenId] as const,
   borrowAmount: (HERC20ContractAddress: string, NFTTokenId: string) => ['nft', 'borrow', HERC20ContractAddress, NFTTokenId] as const,
+  totalSupply: (HERC20ContractAddress: string) => ['asset', 'totalSupply', HERC20ContractAddress] as const,
   // NFT section
   listUserNFTs: (walletPublicKey: string, ERC721ContractAddress: string) => ['nft', 'list', walletPublicKey, ERC721ContractAddress] as const,
   listUserCoupons: (HERC20ContractAddress: string, walletPublicKey: string) => ['coupons', 'list', HERC20ContractAddress, walletPublicKey] as const,
