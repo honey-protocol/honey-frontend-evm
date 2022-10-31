c5207efec93e28cbeb5e0fd9061909621aad62f0
daan <danieloshos3@gmail.com>
2022-10-31 17:32:06 +0100

    add dashboard page

--- components/HoneyButtonTabs/HoneyButtonTabs.tsx
+++ components/HoneyButtonTabs/HoneyButtonTabs.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import * as style from './HoneyButtonTabs.css';
+import c from 'classnames';
 
 export interface HoneyButtonTabsProps {
 	items: HoneyButtonTabsItem[];
+	isFullWidth?: boolean;
 	activeItemSlug: string;
 	onClick: (itemSlug: string) => void;
 }
@@ -13,9 +15,22 @@ type HoneyButtonTabsItem = {
 	slug: string;
 };
 
-export const HoneyButtonTabs: FC<HoneyButtonTabsProps> = ({ items, activeItemSlug, onClick }) => (
-	<div className={style.periodSelector}>
-		<div className={style.periodSelectorContent}>
+export const HoneyButtonTabs: FC<HoneyButtonTabsProps> = ({
+	items,
+	activeItemSlug,
+	isFullWidth,
+	onClick
+}) => (
+	<div
+		className={c(style.periodSelector, {
+			[style.fullWidthSelector]: isFullWidth
+		})}
+	>
+		<div
+			className={c(style.periodSelectorContent, {
+				[style.periodSelectorContentResponsibility]: !isFullWidth
+			})}
+		>
 			{items.map((item, index) => {
 				const itemStatus = item.slug == activeItemSlug ? 'active' : 'passive';
 				const isVisibleDivider = items.length - 1 !== index && itemStatus !== 'active';
---
+++ components/HoneyCardsGrid/types.ts
@@ -0,0 +1,48 @@
+import { PositionType } from '../../types/dashboard';
+
+// export type BorrowUserPosition = {
+//   name: string;
+//   price: number;
+//   debt: number;
+//   ir: number;
+//   imageUrl: string;
+//   id: string;
+// };
+
+export type BorrowUserPosition = {
+  name: string;
+  tokenId: string;
+  couponId: string;
+  image: string;
+  riskLvl?: number;
+  debt?: number;
+  available?: number;
+  value?: number;
+};
+
+export type LendUserPosition = {
+  name: string;
+  deposit: number;
+  value: number;
+  available: number;
+  ir: number;
+  imageUrl: string;
+  id: string;
+};
+
+export interface HoneyCardGridProps {
+  borrowPositions: BorrowUserPosition[];
+  lendPositions: LendUserPosition[];
+  onChangePositionType: (value: PositionType) => void;
+  positionType: PositionType;
+}
+
+export interface BorrowPositionCardProps {
+  position: BorrowUserPosition;
+  onSelect: (tokenId: string, couponId: string) => void;
+}
+
+export interface LendPositionCardProps {
+  position: LendUserPosition;
+  onSelect: (id: string) => void;
+}
