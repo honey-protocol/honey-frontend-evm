462fe4782a15aa1aa8a5563da59e5693751c1051
daan <danieloshos3@gmail.com>
2022-11-14 13:15:46 +0100

    mobile fixes

--- pages/borrow/index.tsx
+++ pages/borrow/index.tsx
@@ -43,6 +43,7 @@ import MarketsSidebar from "../../components/MarketsSidebar/MarketsSidebar";
 import useDisplayStore from "../../store/displayStore";
 import { getContractsByHTokenAddr } from "../../helpers/generalHelper";
 import HealthLvl from 'components/HealthLvl/HealthLvl';
+import c from 'classnames';
 
 const {formatPercent: fp, formatERC20: fs} = formatNumber
 const Markets: NextPage = () => {
@@ -174,7 +175,7 @@ const Markets: NextPage = () => {
                 className={
                   style.headerCell[
                     sortOrder === 'disabled' ? 'disabled' : 'active'
-                    ]
+                  ]
                 }
               >
                 <span>Interest rate</span>
@@ -203,7 +204,7 @@ const Markets: NextPage = () => {
                 className={
                   style.headerCell[
                     sortOrder === 'disabled' ? 'disabled' : 'active'
-                    ]
+                  ]
                 }
               >
                 <span>SUPPLIED</span>
@@ -226,7 +227,7 @@ const Markets: NextPage = () => {
                 className={
                   style.headerCell[
                     sortOrder === 'disabled' ? 'disabled' : 'active'
-                    ]
+                  ]
                 }
               >
                 <span>Available</span>{' '}
@@ -298,6 +299,7 @@ const Markets: NextPage = () => {
 
               <HoneyTableRow>
                 <div className={style.rateCell}>{fp(row.rate * 100)}</div>
+                <div className={style.availableCell}>{fs(row.value)}</div>
                 <div className={style.availableCell}>{fs(row.available)}</div>
               </HoneyTableRow>
             </>
@@ -389,26 +391,8 @@ const Markets: NextPage = () => {
           </div>
           <div className={style.nameCellText}>
             <div className={style.collectionNameMobile}>{row['name']}</div>
-            <div className={style.risk.safe}>
-              <span className={style.valueCell}>{0}</span>
-            </div>
+            <HealthLvl healthLvl={row.healthLvl || 0} />
           </div>
-        </div>
-      )
-    },
-    {
-      dataIndex: 'debt',
-      render: debt => (
-        <div className={style.expandedRowCell}>
-          <InfoBlock title={'Debt:'} value={'0'}/>
-        </div>
-      )
-    },
-    {
-      dataIndex: 'available',
-      render: available => (
-        <div className={style.expandedRowCell}>
-          <InfoBlock title={'Allowance:'} value={'0'}/>
         </div>
       )
     },
@@ -418,7 +402,7 @@ const Markets: NextPage = () => {
       title: '',
       width: '50px',
       render: (text, row) => (
-        <div className={style.buttonsCell}>
+        <div className={c(style.expandedRowCell, style.buttonsCell)}>
           <HoneyButton
             variant="text"
             onClick={e => initLoanOrBorrowFlow(row['tokenId'], row['couponId'])}
@@ -482,15 +466,6 @@ const Markets: NextPage = () => {
         </Typography.Text>
       </div>
       <HoneyContent sidebar={marketSidebar()}>
-        <div className={style.mobileTableHeader}>
-          <div className={style.mobileRow}>
-            <SearchForm/>
-          </div>
-          <div className={style.mobileRow}>
-            <MyCollectionsToggle/>
-          </div>
-        </div>
-
         <div className={style.hideTablet}>
           <HoneyTable
             hasRowsShadow={true}
@@ -537,6 +512,24 @@ const Markets: NextPage = () => {
         </div>
 
         <div className={style.showTablet}>
+          <div
+            className={c(
+              style.mobileTableHeader,
+              style.mobileSearchAndToggleContainer
+            )}
+          >
+            <div className={style.mobileRow}>
+              <SearchForm />
+            </div>
+            <div className={style.mobileRow}>
+              <MyCollectionsToggle />
+            </div>
+          </div>
+          <div className={c(style.mobileTableHeader)}>
+            <div className={style.tableCell}>Interest</div>
+            <div className={style.tableCell}>Supplied</div>
+            <div className={style.tableCell}>Available</div>
+          </div>
           <HoneyTable
             hasRowsShadow={true}
             tableLayout="fixed"
@@ -581,23 +574,23 @@ const Markets: NextPage = () => {
           />
         </div>
         {!tableDataFiltered.length &&
-        (isMyCollectionsFilterEnabled ? (
-          <div className={style.emptyStateContainer}>
-            <EmptyStateDetails
+          (isMyCollectionsFilterEnabled ? (
+            <div className={style.emptyStateContainer}>
+              <EmptyStateDetails
               icon={<div className={style.docIcon}/>}
-              title="You didn’t use any collections yet"
-              description="Turn off the filter my collection and choose any collection to borrow money"
-            />
-          </div>
-        ) : (
-          <div className={style.emptyStateContainer}>
-            <EmptyStateDetails
+                title="You didn’t use any collections yet"
+                description="Turn off the filter my collection and choose any collection to borrow money"
+              />
+            </div>
+          ) : (
+            <div className={style.emptyStateContainer}>
+              <EmptyStateDetails
               icon={<div className={style.docIcon}/>}
-              title="No collections to display"
-              description="Turn off all filters and clear search inputs"
-            />
-          </div>
-        ))}
+                title="No collections to display"
+                description="Turn off all filters and clear search inputs"
+              />
+            </div>
+          ))}
       </HoneyContent>
     </LayoutRedesign>
   );
