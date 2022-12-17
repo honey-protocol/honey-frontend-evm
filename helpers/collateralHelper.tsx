9915da63b5481b646d63827b302cc388b4db65e9
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2022-12-17 17:55:44 -0500

    bug fix

--- helpers/collateralHelper.tsx
+++ helpers/collateralHelper.tsx
@@ -2,14 +2,14 @@ import { collections } from '../constants/NFTCollections';
 
 export function getNFTName(HERC20ContractAddress: string) {
 	const collection = collections.find(
-		(collection) => (collection.HERC20ContractAddress = HERC20ContractAddress)
+		(collection) => collection.HERC20ContractAddress == HERC20ContractAddress
 	);
 	return collection?.name || '';
 }
 
 export function getNFTDefaultImage(HERC20ContractAddress: string) {
 	const collection = collections.find(
-		(collection) => (collection.HERC20ContractAddress = HERC20ContractAddress)
+		(collection) => collection.HERC20ContractAddress == HERC20ContractAddress
 	);
 	return collection?.icon || '';
 }
