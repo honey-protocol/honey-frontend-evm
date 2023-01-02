04a6a989e42bf9df092eade6048ea7f5601d16f3
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2023-01-01 19:36:00 -0500

    remove warning message

--- components/HoneyCardsGrid/BorrowPositionCard/BorrowPositionCard.tsx
+++ components/HoneyCardsGrid/BorrowPositionCard/BorrowPositionCard.tsx
@@ -16,40 +16,37 @@ const { formatUsd: fu, formatPercent: fp } = formatNumber;
 const MAX_LTV = 0.5;
 const LIQUIDATION_THRESHOLD = 0.65;
 
-export const BorrowPositionCard: FC<BorrowPositionCardProps> = ({
-  position,
-  onSelect
-}) => {
-  const selectedNFTId = useLoanFlowStore(state => state.NFTId);
-  if (!position) return null;
-  return (
-    <div
-      className={c(styles.positionCard, {
-        [styles.activeCard]: selectedNFTId === position.tokenId
-      })}
-      onClick={() => onSelect(position.tokenId, position.couponId)}
-    >
-      <div className={styles.collectionIcon}>
-        <HexaBoxContainer>
-          <Image width={46} height={46} src={position.image} />
-        </HexaBoxContainer>
-      </div>
-      <div className={styles.positionName}>
-        <HoneyTooltip label={position.name}>{fnn(position.name)}</HoneyTooltip>
-        <div className={styles.arrowIcon} />
-      </div>
-      <div className={styles.positionValues}>
-        <InfoBlock title="Floor price" value={fu(position.value)} />
-        <InfoBlock title="Debt" value={fu(position.debt)} />
-        <InfoBlock title="IR" value={fp((position.riskLvl ?? 0) * 100)} />
-      </div>
-      <div className={styles.divider} />
-      <BorrowPositionCardSlider
-        debt={position.debt ?? 0}
-        collateralValue={position.value ?? 0}
-        liquidationThreshold={LIQUIDATION_THRESHOLD}
-        maxLoanToValue={MAX_LTV}
-      />
-    </div>
-  );
+export const BorrowPositionCard: FC<BorrowPositionCardProps> = ({ position, onSelect }) => {
+	const selectedNFTId = useLoanFlowStore((state) => state.NFTId);
+	if (!position) return null;
+	return (
+		<div
+			className={c(styles.positionCard, {
+				[styles.activeCard]: selectedNFTId === position.tokenId
+			})}
+			onClick={() => onSelect(position.tokenId, position.couponId)}
+		>
+			<div className={styles.collectionIcon}>
+				<HexaBoxContainer>
+					<Image width={46} height={46} src={position.image} alt={'collection Icon'} />
+				</HexaBoxContainer>
+			</div>
+			<div className={styles.positionName}>
+				<HoneyTooltip label={position.name}>{fnn(position.name)}</HoneyTooltip>
+				<div className={styles.arrowIcon} />
+			</div>
+			<div className={styles.positionValues}>
+				<InfoBlock title="Floor price" value={fu(position.value)} />
+				<InfoBlock title="Debt" value={fu(position.debt)} />
+				<InfoBlock title="IR" value={fp((position.riskLvl ?? 0) * 100)} />
+			</div>
+			<div className={styles.divider} />
+			<BorrowPositionCardSlider
+				debt={position.debt ?? 0}
+				collateralValue={position.value ?? 0}
+				liquidationThreshold={LIQUIDATION_THRESHOLD}
+				maxLoanToValue={MAX_LTV}
+			/>
+		</div>
+	);
 };
--- components/HoneyCardsGrid/LendPositionCard/LendPositionCard.tsx
+++ components/HoneyCardsGrid/LendPositionCard/LendPositionCard.tsx
@@ -11,38 +11,33 @@ import useLoanFlowStore from 'store/loanFlowStore';
 
 const { formatUsd: fu, formatPercent: fp } = formatNumber;
 
-export const LendPositionCard: FC<LendPositionCardProps> = ({
-  position,
-  onSelect
-}) => {
-  const selectedNFTId = useLoanFlowStore(state => state.NFTId);
-  return (
-    <div
-      className={c(styles.positionCard, {
-        [styles.activeCard]: selectedNFTId === position.id
-      })}
-      onClick={() => onSelect(position.id)}
-    >
-      <div className={styles.collectionIcon}>
-        <HexaBoxContainer>
-          <Image width={46} height={46} src={position.imageUrl} />
-        </HexaBoxContainer>
-      </div>
-      <div className={styles.positionName}>
-        <HoneyTooltip label={position.name}>{fnn(position.name)}</HoneyTooltip>
-        <div className={styles.arrowIcon} />
-      </div>
-      <div className={styles.positionValues}>
-        <InfoBlock
-          title="IR"
-          value={
-            <span className={styles.irValue}>{fp(position.ir * 100)}</span>
-          }
-        />
-        <InfoBlock title="Your Deposit" value={fu(position.deposit)} />
-        <InfoBlock title="Value" value={fu(position.value)} />
-        <InfoBlock title="Available" value={fu(position.available)} />
-      </div>
-    </div>
-  );
+export const LendPositionCard: FC<LendPositionCardProps> = ({ position, onSelect }) => {
+	const selectedNFTId = useLoanFlowStore((state) => state.NFTId);
+	return (
+		<div
+			className={c(styles.positionCard, {
+				[styles.activeCard]: selectedNFTId === position.id
+			})}
+			onClick={() => onSelect(position.id)}
+		>
+			<div className={styles.collectionIcon}>
+				<HexaBoxContainer>
+					<Image width={46} height={46} src={position.imageUrl} alt={'collection Icon'} />
+				</HexaBoxContainer>
+			</div>
+			<div className={styles.positionName}>
+				<HoneyTooltip label={position.name}>{fnn(position.name)}</HoneyTooltip>
+				<div className={styles.arrowIcon} />
+			</div>
+			<div className={styles.positionValues}>
+				<InfoBlock
+					title="IR"
+					value={<span className={styles.irValue}>{fp(position.ir * 100)}</span>}
+				/>
+				<InfoBlock title="Your Deposit" value={fu(position.deposit)} />
+				<InfoBlock title="Value" value={fu(position.value)} />
+				<InfoBlock title="Available" value={fu(position.available)} />
+			</div>
+		</div>
+	);
 };
