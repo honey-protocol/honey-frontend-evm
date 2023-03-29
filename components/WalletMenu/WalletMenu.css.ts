b1ae483dba4296d67f9773544c27491cec207014
daan <danieloshos3@gmail.com>
2023-03-29 10:12:30 +0100

    chain select dropdown on header

--- components/WalletMenu/WalletMenu.css.ts
+++ components/WalletMenu/WalletMenu.css.ts
@@ -1,4 +1,4 @@
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 import { typography, vars } from '../../styles/theme.css';
 
 export const title = style({
@@ -18,3 +18,42 @@ export const settingsIcon = style({
 	width: 'unset',
 	marginRight: 10
 });
+
+export const dropdownSelect = style({
+	// background: 'red'
+	width: 150
+});
+
+globalStyle(`${dropdownSelect} .ant-select-selector`, {
+	borderRadius: '10px !important',
+	fontFamily: 'Scandia',
+	fontWeight: '500 !important'
+});
+
+globalStyle(`${dropdownSelect} .ant-space-item `, {
+	display: 'flex'
+});
+
+globalStyle(`${dropdownSelect} .ant-select-selector svg`, {
+	width: 20,
+	height: 20
+});
+
+globalStyle(`${dropdownSelect} .ant-select-selector svg path`, {
+	fill: 'var(--colors-text)'
+});
+
+export const selectDropdownList = style({});
+
+globalStyle(`${selectDropdownList} svg`, {
+	width: 20,
+	height: 20
+});
+
+export const modalTitle = style([
+	typography.title,
+	{
+		display: 'flex',
+		justifyContent: 'center'
+	}
+]);
