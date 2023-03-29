9a531571fbc2babbc9eec76abf9726580c76735a
BowTiedHeron <bowtiedheron@protonmail.com>
2023-03-29 08:37:10 +0200

    add markets & icons

--- constants/NFTCollections.tsx
+++ constants/NFTCollections.tsx
@@ -12,7 +12,38 @@ function getCollections() {
 				ERC721ContractAddress: '0x9d305a42A3975Ee4c1C57555BeD5919889DCE63F',
 				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
 				HERC20ContractAddress: '0x9e826d1427c15ff6f41521e9811fd526f7717a2a'
+			},
+			{
+				name: 'Y00TS',
+				icon: '/nfts/y00ts.png',
+				erc20Name: 'WETH',
+				erc20Icon: '/erc20/WethIcon.png',
+				unit: 'ether',
+				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
+				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
 			}
+			// ,
+			// {
+			// 	name: 'OWLPHA',
+			// 	icon: '/nfts/owlpha.png',
+			// 	erc20Name: 'MATIC',
+			// 	erc20Icon: '/erc20/MaticIcon.png',
+			// 	unit: 'ether',
+			// // 	ERC721ContractAddress: '',
+			// // 	ERC20ContractAddress: '',
+			// // 	HERC20ContractAddress: ''
+			// },
+			// {
+			// 	name: 'BROZO',
+			// 	icon: '/nfts/brozo.png',
+			// 	erc20Name: 'MATIC',
+			// 	erc20Icon: '/erc20/MaticIcon.png',
+			// 	unit: 'ether',
+			// 	// ERC721ContractAddress: '',
+			// 	// ERC20ContractAddress: '',
+			// 	// HERC20ContractAddress: ''
+			// }
 		] as collection[];
 	} else if (envName == 'prod') {
 		// change this prod market when we go to prod
@@ -26,7 +57,38 @@ function getCollections() {
 				ERC721ContractAddress: '0x9d305a42A3975Ee4c1C57555BeD5919889DCE63F',
 				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
 				HERC20ContractAddress: '0x9e826d1427c15ff6f41521e9811fd526f7717a2a'
+			},
+			{
+				name: 'Y00TS',
+				icon: '/nfts/y00ts.png',
+				erc20Name: 'WETH',
+				erc20Icon: '/erc20/WethIcon.png',
+				unit: 'ether',
+				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
+				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
 			}
+			// ,
+			// {
+			// 	name: 'OWLPHA',
+			// 	icon: '/nfts/owlpha.png',
+			// 	erc20Name: 'MATIC',
+			// 	erc20Icon: '/erc20/MaticIcon.png',
+			// 	unit: 'ether',
+			// // 	ERC721ContractAddress: '',
+			// // 	ERC20ContractAddress: '',
+			// // 	HERC20ContractAddress: ''
+			// },
+			// {
+			// 	name: 'BROZO',
+			// 	icon: '/nfts/brozo.png',
+			// 	erc20Name: 'MATIC',
+			// 	erc20Icon: '/erc20/MaticIcon.png',
+			// 	unit: 'ether',
+			// 	// ERC721ContractAddress: '',
+			// 	// ERC20ContractAddress: '',
+			// 	// HERC20ContractAddress: ''
+			// }
 		] as collection[];
 	} else {
 		return [] as collection[];
