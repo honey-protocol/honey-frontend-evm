a2bb605dad6ad1b8910d8c408af3c20fb131a04a
BowTiedHeron <bowtiedheron@protonmail.com>
2023-03-31 17:00:11 +0200

    update allowance text

--- components/RepayForm/RepayForm.tsx
+++ components/RepayForm/RepayForm.tsx
@@ -345,7 +345,7 @@ const RepayForm = (props: RepayProps) => {
 								</span>
 							}
 							toolTipLabel={`Allowance determines how much debt is available to a borrower. This market supports no more than ${fp(
-								60
+								40
 							)}`}
 						/>
 					</div>
