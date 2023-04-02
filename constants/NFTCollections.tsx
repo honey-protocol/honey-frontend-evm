e174fb73cce33cd364b84e38bf020ebbc2d3635b
BowTiedHeron <bowtiedheron@protonmail.com>
2023-04-02 14:33:54 +0200

    test derby market

--- constants/NFTCollections.tsx
+++ constants/NFTCollections.tsx
@@ -4,6 +4,16 @@ function getCollections() {
 	if (envName == 'dev') {
 		return [
 			{
+				name: 'Y00TS',
+				icon: '/nfts/y00ts.png',
+				erc20Name: 'WETH',
+				erc20Icon: '/erc20/EthIcon.png',
+				unit: 'ether',
+				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
+				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
+			},
+			{
 				name: 'LAND',
 				icon: '/nfts/sandbox.png',
 				erc20Name: 'WETH',
@@ -14,14 +24,14 @@ function getCollections() {
 				HERC20ContractAddress: '0x9e826d1427c15ff6f41521e9811fd526f7717a2a'
 			},
 			{
-				name: 'Y00TS',
-				icon: '/nfts/y00ts.png',
-				erc20Name: 'WETH',
-				erc20Icon: '/erc20/EthIcon.png',
+				name: 'Chicken-Derby',
+				icon: '/nfts/derby.png',
+				erc20Name: 'MATIC',
+				erc20Icon: '/erc20/MaticIcon.png',
 				unit: 'ether',
-				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
-				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
-				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
+				ERC721ContractAddress: '0x8634666bA15AdA4bbC83B9DbF285F73D9e46e4C2',
+				ERC20ContractAddress: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+				HERC20ContractAddress: '0x6e72368737f628db71336eec98e99d8496257965'
 			}
 			// ,
 			// {
@@ -49,6 +59,16 @@ function getCollections() {
 		// change this prod market when we go to prod
 		return [
 			{
+				name: 'Y00TS',
+				icon: '/nfts/y00ts.png',
+				erc20Name: 'WETH',
+				erc20Icon: '/erc20/EthIcon.png',
+				unit: 'ether',
+				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
+				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
+			},
+			{
 				name: 'LAND',
 				icon: '/nfts/sandbox.png',
 				erc20Name: 'WETH',
@@ -59,14 +79,14 @@ function getCollections() {
 				HERC20ContractAddress: '0x9e826d1427c15ff6f41521e9811fd526f7717a2a'
 			},
 			{
-				name: 'Y00TS',
-				icon: '/nfts/y00ts.png',
-				erc20Name: 'WETH',
-				erc20Icon: '/erc20/EthIcon.png',
+				name: 'Chicken-Derby',
+				icon: '/nfts/derby.png',
+				erc20Name: 'MATIC',
+				erc20Icon: '/erc20/MaticIcon.png',
 				unit: 'ether',
-				ERC721ContractAddress: '0x670fd103b1a08628e9557cD66B87DeD841115190',
-				ERC20ContractAddress: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
-				HERC20ContractAddress: '0x13e3b746e89fd9693222fd377521d874e0e1ae5b'
+				ERC721ContractAddress: '0x8634666bA15AdA4bbC83B9DbF285F73D9e46e4C2',
+				ERC20ContractAddress: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+				HERC20ContractAddress: '0x6e72368737f628db71336eec98e99d8496257965'
 			}
 			// ,
 			// {
