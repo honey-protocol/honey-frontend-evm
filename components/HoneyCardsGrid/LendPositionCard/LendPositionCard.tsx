36eb0034e8bf2ee3bed96d531085b8fd89b03198
Daniel Oshioke <danieloshos3@gmail.com>
2023-04-24 20:32:47 +0100

    Fix: Dashboard UX upgrades (#120)
    * dashboard search
    
    * sorting positions
    
    * fix zero debt loans showing twice
    
    * fix lend sort order
    
    * use hook instead of subgraph value for borrow amt
    
    * show token icon instead of name

--- components/HoneyCardsGrid/LendPositionCard/LendPositionCard.tsx
+++ components/HoneyCardsGrid/LendPositionCard/LendPositionCard.tsx
@@ -8,11 +8,14 @@ import { formatNumber, formatNFTName as fnn } from '../../../helpers/format';
 import c from 'classnames';
 import HoneyTooltip from '../../HoneyTooltip/HoneyTooltip';
 import useLendFlowStore from 'store/lendFlowStore';
+import { getContractsByHTokenAddr } from 'helpers/generalHelper';
 
 const { formatShortName: fsn, formatPercent: fp } = formatNumber;
 
 export const LendPositionCard: FC<LendPositionCardProps> = ({ position, onSelect }) => {
 	const selectedMarket = useLendFlowStore((state) => state.HERC20ContractAddr);
+
+	const { erc20Name, erc20Icon, formatDecimals } = getContractsByHTokenAddr(position.id);
 	return (
 		<div
 			className={c(styles.positionCard, {
@@ -31,9 +34,28 @@ export const LendPositionCard: FC<LendPositionCardProps> = ({ position, onSelect
 			</div>
 			<div className={styles.positionValues}>
 				<InfoBlock title="IR" value={<span className={styles.irValue}>{fp(position.rate)}</span>} />
-				<InfoBlock title="Your Deposit" value={fsn(parseFloat(position.deposit))} />
-				<InfoBlock title="Supplied" value={fsn(position.supplied)} />
-				<InfoBlock title="Available" value={fsn(position.available)} />
+				<InfoBlock
+					title="Your Deposit"
+					value={fsn(parseFloat(position.deposit < '0' ? '0' : position.deposit))}
+				/>
+				<InfoBlock
+					title="Supplied"
+					value={
+						<div className={styles.infoRow}>
+							{fsn(position.supplied, formatDecimals)}
+							<Image src={erc20Icon} alt={erc20Name} layout="fixed" width="16px" height="16px" />
+						</div>
+					}
+				/>
+				<InfoBlock
+					title="Available"
+					value={
+						<div className={styles.infoRow}>
+							{fsn(position.available, formatDecimals)}
+							<Image src={erc20Icon} alt={erc20Name} layout="fixed" width="16px" height="16px" />
+						</div>
+					}
+				/>
 			</div>
 		</div>
 	);
